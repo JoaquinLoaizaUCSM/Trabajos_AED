7360833fa5b84af9f40ee69c2a50f34f1238305f
Elizabeth Camila Rodriguez <ecamilarl@gmail.com>
2025-03-13 14:01:53 -0500

    comments clase coordenada

--- Semana_1/Actividades/Coordenada.java
+++ Semana_1/Actividades/Coordenada.java
@@ -1,24 +1,29 @@
 public class Coordenada{
-    
+
+    // atributos
     private double x;
     private double y;
 
+    // Constructor1
     public Coordenada( ){
         this.x = 0.0;
         this.y = 0.0;
     }
 
+    // Constructor2
     public Coordenada(double x, double y ){
         this.x = x;
         this.y = y;
     }
 
+    // Constructor3
+    // sobrecarga de constructores
     public Coordenada(Coordenada c ){
         this.x = c.x;
         this.y = c.y;
     }
 
-   
+   // setters
     void setX(double x) {
         this.x = x;
     }
@@ -27,7 +32,7 @@ public class Coordenada{
         this.y = y;
     }
 
-    
+    // getters
     double getX(){
         return this.x;
     }
@@ -36,14 +41,14 @@ public class Coordenada{
         return this.y;
     }
 
-   
+    // metodo de instancia que calcula la distancia euclideana
     double distancia(Coordenada c){
-        double dx = this.x - c.x;
-        double dy = this.y - c.y;
-        return Math.sqrt(dx*dx + dy*dy);
+        double dx = this.x - c.x;  // (x2 - x1)
+        double dy = this.y - c.y;  // (y2 - y1)
+        return Math.sqrt(dx*dx + dy*dy); // √(dx² + dy²)
     }
 
-   
+   // metodo de clase que calcula la distancia euclideana
     static double distancia(Coordenada c1, Coordenada c2){
         double dx = c1.x - c2.x;
         double dy = c1.y - c2.y;
