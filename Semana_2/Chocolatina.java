663112c0271dc54dbedb54f8028e552c1df9568e
jhosfernan12 <jhosfernan12@gmail.com>
2025-03-25 20:16:58 -0500

    Clase Chocolatina Actualizada

--- Semana_2/Chocolatina.java
+++ Semana_2/Chocolatina.java
@@ -1,17 +1,34 @@
-package Semana_2;
 
-public class Chocolatina { 
-    private String marca; 
-    
-    public Chocolatina(String marca) { 
-        this.marca = marca; 
-    } 
-    
-    public String getMarca() { 
-        return marca; 
-    } 
-    
-    public void setMarca(String marca) { 
-        this.marca = marca; 
-    } 
+public class Chocolatina 
+{
+    private String marca;
+
+    public Chocolatina(String marca) 
+    {
+        this.marca = marca;
+    }
+
+    public String getMarca() 
+    {
+        return marca;
+    }
+
+    public void setMarca(String marca) 
+    {
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return "Chocolatina " + marca;
+    }
+
+    @Override
+    public boolean equals(Object obj) //sirve para comparar si dos objetos son iguales segun sus atributos (marca) en lugar de su posición en memoria
+    {
+        if (this == obj) return true;                 // Son el mismo objeto
+        if (obj == null || getClass() != obj.getClass()) return false; // Tipos diferentes
+        Chocolatina that = (Chocolatina) obj;         // Cast seguro
+        return marca.equals(that.marca);              // Comparación por marca
+    }
 }
