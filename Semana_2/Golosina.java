823d1e9befce543ed07ebcffabb99058e1cd78e2
jhosfernan12 <jhosfernan12@gmail.com>
2025-03-26 20:17:15 -0500

    Clase Golosina Actualizada

--- Semana_2/Golosina.java
+++ Semana_2/Golosina.java
@@ -1,28 +1,47 @@
-package Semana_2;
+public class Golosina 
+{
+    private String nombre;
+    private double peso;
 
-public class Golosina { 
-    private String nombre; 
-    private double peso; 
-    
-    public Golosina(String nombre, double peso) { 
-        this.nombre = nombre; 
-        this.peso = peso; 
-    } 
-    
-    public String getNombre() { 
-        return this.nombre; 
-    } 
-    
-    public void setNombre(String nombre) { 
-    
-        this.nombre = nombre; 
-    } 
-    
-    public double getPeso() { 
-        return this.peso; 
-    } 
-    
-    public void setPeso(double peso) { 
-        this.peso = peso; 
-    } 
+    public Golosina(String nombre, double peso) 
+    {
+        this.nombre = nombre;
+        this.peso = peso;
+    }
+
+    public String getNombre() 
+    {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre)
+     {
+        this.nombre = nombre;
+    }
+
+    public double getPeso() 
+    {
+        return this.peso;
+    }
+
+    public void setPeso(double peso) 
+    {
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() 
+    {
+        return nombre + " (" + peso + " kg)";
+    }
+
+    @Override
+    public boolean equals(Object obj) //sirve para comparar si dos objetos son iguales segun sus atributos (nombre y peso) en lugar de su posición en memoria
+    {
+        if (this == obj) return true;                 // Misma instancia
+        if (obj == null || getClass() != obj.getClass()) return false; // Tipos diferentes
+        Golosina golosina = (Golosina) obj;           // Cast seguro
+        return Double.compare(golosina.peso, peso) == 0 && // Compara peso
+               nombre.equals(golosina.nombre);        // y nombre
+    }
 }
