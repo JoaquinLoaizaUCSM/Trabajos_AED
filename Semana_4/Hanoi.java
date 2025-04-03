ae2d2b893e347695d9b13832643988864730957a
JoaquinLoaizaUCSM <134557588+JoaquinLoaizaUCSM@users.noreply.github.com>
2025-04-03 23:43:25 +0000

    Se subio la actividad 1 se la semana_4

---
+++ Semana_4/Hanoi.java
@@ -0,0 +1,27 @@
+package Semana_4;
+
+public class Hanoi {
+
+    public static void main(String[] args) {
+        System.out.println("Total de movientes reazados: " + resoluHanoi(5, 'O', 'T', 'D'));
+        
+    }
+    public static int resoluHanoi(int disco, char torre1, char torre2, char torre3){
+        int contador = 0;
+
+        if (disco == 1){
+            System.out.println("Se mueve disco "+disco+" de torre "+ torre1 + " a torre " + torre3);
+            contador += 1;
+        }
+
+        else{
+            contador += resoluHanoi(disco -1 ,  torre1, torre2, torre3);
+            resoluHanoi(disco -1 , torre1, torre2, torre3);
+            System.out.println("Se mueve disco " + disco + " de torre "+ torre1 + " a torre " + torre3);
+            contador += 1;
+            contador += resoluHanoi(disco -1, torre1, torre2, torre3);
+        }
+
+        return contador;
+    }
+}
