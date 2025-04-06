71c02281fcd232bb6497affbdfdd34c7242101e7
jhosfernan12 <jhosfernan12@gmail.com>
2025-04-06 17:37:41 -0500

    Moda 1 Actuailizada con comentarios

--- Semana_4/moda1.java
+++ Semana_4/moda1.java
@@ -4,14 +4,14 @@ public static int moda1(int array[])
     int end = array.length-1;
     if (first == end)
     {
-        return array[first];
+        return array[first]; // Si el arreglo solo tiene 1 elemento, esa es la moda
     }
-    int moda = array[first];
-    int maxfrec = frecuencia(array, first, end, moda);
-    for (int i = first+1; i<end; i++)
+    int moda = array[first]; // Inicializa la moda como el primer elemento
+    int maxfrec = frecuencia(array, first, end, moda); // Calcula la frecuencia del primer elemento
+    for (int i = first+1; i<end; i++)  // Recorre el arreglo desde el segundo elemento
     {
-        int frec = frecuencia(array, i, end, array[i]);
-        if(frec > maxfrec)
+        int frec = frecuencia(array, i, end, array[i]); // Calcula la frecuencia de cada elemento
+        if(frec > maxfrec)// Si la frecuencia es mayor a la maxima frecuencia, actualiza la moda
         {
             maxfrec = frec;
             moda = array[i];
@@ -22,11 +22,11 @@ public static int moda1(int array[])
 
 private static int frecuencia(int []array, int first, int end, int ele)
 {
-    if(first > end){return 0;}
-    int suma = 0;
+    if(first > end) {return 0;}  // Si el rango no es valido, devuelve 0
+    int suma = 0; 
     for (int i = first; i <= end; i++)
     {
-        if (array[i] == ele) {suma ++;}
+        if (array[i] == ele) {suma ++;}  // Cuenta cuantas veces aparece el elemento
     }
-    return suma;
+    return suma;// Devuelve la frecuencia de ese elemento (cuantas veces lo conto)
 }
