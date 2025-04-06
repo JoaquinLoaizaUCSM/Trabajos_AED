b0435c9cd32750c136964c6a15b3f8a1d61fee53
KyraEgon <36451562+jhosfernan12@users.noreply.github.com>
2025-04-06 17:02:51 -0500

    Hanoi Corregido.java
    Ya funciona! :D

--- Semana_4/Hanoi.java
+++ Semana_4/Hanoi.java
@@ -3,24 +3,21 @@ package Semana_4;
 public class Hanoi {
 
     public static void main(String[] args) {
-        System.out.println("Total de movimientos realizados: " + resoluHanoi(5, 'O', 'T', 'D'));
-        
+        System.out.println("Total de movimientos realizados: " + resoluHanoi(5, 'A', 'B', 'C'));
     }
-    public static int resoluHanoi(int disco, char torre1, char torre2, char torre3){
+
+    public static int resoluHanoi(int discos, char torre1, char torre2, char torre3) {
         int contador = 0;
 
-        if (disco == 1){
-            System.out.println("Se mueve disco "+disco+" de torre "+ torre1 + " a torre " + torre3);
-            contador += 1;
+        if (discos == 1) {
+            System.out.println("Se mueve disco " + discos + " de torre " + torre1 + " a torre " + torre3);
+            return 1; 
         }
 
-        else{
-            contador += resoluHanoi(disco -1 ,  torre1, torre2, torre3);
-            resoluHanoi(disco -1 , torre1, torre2, torre3);
-            System.out.println("Se mueve disco " + disco + " de torre "+ torre1 + " a torre " + torre3);
-            contador += 1;
-            contador += resoluHanoi(disco -1, torre1, torre2, torre3);
-        }
+        contador += resoluHanoi(discos - 1, torre1, torre3, torre2);
+        System.out.println("Se mueve disco " + discos + " de torre " + torre1 + " a torre " + torre3);
+        contador += 1;
+        contador += resoluHanoi(discos - 1, torre2, torre1, torre3);
 
         return contador;
     }
