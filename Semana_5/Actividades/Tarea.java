607940b249ed94c0a0704f09840d3b77acb16219
Elizabeth Camila Rodriguez <ecamilarl@gmail.com>
2025-04-28 06:35:22 -0500

    update Tarea.java

--- Semana_5/Actividades/Tarea.java
+++ Semana_5/Actividades/Tarea.java
@@ -30,20 +30,37 @@ public class Tarea implements Comparable<Tarea> {
 
     @Override
     public int compareTo(Tarea o) {
-        int cmp = Integer.compare(this.prioridad, o.prioridad);
-        return (cmp != 0) ? cmp : this.titulo.compareToIgnoreCase(o.titulo);
+        // Primero compara las prioridades de las tareas
+        int cmp = Integer.compare(this.prioridad, o.prioridad); // cmp puede ser -1,1,0
+        
+        // Si las prioridades son diferentes, devuelve el resultado de la comparación
+        // Si las prioridades son iguales (cmp == 0), compara los títulos ignorando mayúsculas/minúsculas
+        return (cmp == 0) ? this.titulo.compareToIgnoreCase(o.titulo) : cmp;
     }
-
+    
     @Override
     public boolean equals(Object o) {
+        // Verificación rápida: si son el mismo objeto en memoria, son iguales
         if (this == o) return true;
+        
+        // si el Object  no es una instancia de Tarea devuelve false
         if (!(o instanceof Tarea)) return false;
+        
+        // Convierte el Object del parametro a tipo Tarea para acceder a sus propiedades
         Tarea other = (Tarea) o;
+        
+        // Caso 1: this.titulo no es nulo comparo su contenido 
         if (titulo != null) {
-            if (!titulo.equals(other.titulo)) return false;
+            //  // se compararan los títulos
+            if (!titulo.equalsIgnoreCase(other.titulo)) return false;
+            // Caso 2: this.titulo es nulo, pero other.titulo no es nulo
         } else if (other.titulo != null) {
+            // las tareas no son iguales
             return false;
         }
+        
+        // Finalmente, compara las prioridades
+        // Las tareas son iguales solo si tienen mismo título y misma prioridad
         return prioridad == other.prioridad;
     }
 }
