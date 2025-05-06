82c0a01119ff324616bb1c48edc756f3cfc2c96d
Elizabeth Camila Rodriguez <ecamilarl@gmail.com>
2025-05-06 18:31:20 -0500

    interfaz Stack creada

---
+++ Semana_6/actividades/ExceptionIsEmpty.java
@@ -0,0 +1,13 @@
+package actividades;
+
+public class ExceptionIsEmpty extends RuntimeException {
+
+    // Constructor por defecto con mensaje genérico
+    public ExceptionIsEmpty() {
+        super("Está vacía.");
+    }
+    // Constructor con mensaje personalizado
+    public ExceptionIsEmpty(String message) {
+        super(message);
+    }
+}
---
+++ Semana_6/actividades/Stack.java
@@ -0,0 +1,8 @@
+package actividades;
+
+public interface Stack<E> {
+    void push(E x);
+    E pop() throws ExceptionIsEmpty;
+    E top() throws ExceptionIsEmpty;
+    boolean isEmpty();
+}
