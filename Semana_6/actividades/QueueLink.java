69773a5811be4dbc0d342fbddd25403497cba8e0
Elizabeth Camila Rodriguez <ecamilarl@gmail.com>
2025-05-07 09:39:19 -0500

    actividad2 agregado

---
+++ Semana_6/actividades/QueueLink.java
@@ -0,0 +1,70 @@
+package actividades;
+
+public class QueueLink<E> implements Queue<E> {
+    private Node<E> first;
+    private Node<E> last;
+
+    public QueueLink() {
+        this.first = null;
+        this.last = null;
+    }
+
+    @Override
+    public void enqueue(E x) {
+        Node<E> aux = new Node<>(x);
+        if (this.isEmpty()) {
+            this.first = aux;
+        } else {
+            this.last.setNext(aux);
+        }
+        this.last = aux;
+    }
+
+    @Override
+    public E dequeue() throws ExceptionIsEmpty {
+        if (this.isEmpty()) {
+            throw new ExceptionIsEmpty("No se puede desencolar: cola vacía.");
+        }
+        E data = this.first.getData();
+        this.first = this.first.getNext();
+        if (this.first == null) {
+            this.last = null;
+        }
+        return data;
+    }
+
+    @Override
+    public E back() throws ExceptionIsEmpty {
+        if (this.isEmpty()) {
+            throw new ExceptionIsEmpty("Cola vacía.");
+        }
+        return this.last.getData();
+    }
+
+    @Override
+    public E front() throws ExceptionIsEmpty {
+        if (this.isEmpty()) {
+            throw new ExceptionIsEmpty("Cola vacía.");
+        }
+        return this.first.getData();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.first == null;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node<E> current = this.first;
+        while (current != null) {
+            sb.append(current.getData());
+            if (current.getNext() != null) {
+                sb.append(" -> ");
+            }
+            current = current.getNext();
+        }
+        return sb.toString();
+    }
+}
